2e9eff536b24a092b8742d11027f5a2dc0820d27
MysticalCreator-max <mysticmatrix42@gmail.com>
2026-08-12 10:11:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import nest_asyncio
 nest_asyncio.apply()
 import logging
@@ -11,8 +12,8 @@ from database import init_db
 from user import register_user_handlers
 from admin import register_admin_handlers
 
-# ═══ SOZLAMALAR ═══
-BOT_TOKEN = ""
+# —— SOZLAMALAR ——
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = 6832985197  # O'zingizning ID ingiz
 
 logging.basicConfig(level=logging.INFO)
@@ -20,15 +21,19 @@ logging.basicConfig(level=logging.INFO)
 def main():
     import asyncio
     asyncio.get_event_loop().run_until_complete(init_db())
-    
+
+    if not BOT_TOKEN:
+        raise ValueError("BOT_TOKEN topilmadi! Render Environment Variables qismini tekshiring.")
+
     app = Application.builder().token(BOT_TOKEN).build()
-    
+
     register_admin_handlers(app)
     register_user_handlers(app)
-    
+
     print("🎌 Anime Bot ishga tushdi!")
-    
+
     app.run_polling(allowed_updates=["message", "callback_query"])
 
 if __name__ == "__main__":
     main()
+    
